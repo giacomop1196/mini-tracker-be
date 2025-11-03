8852112dac9861c948c30106f515d43dee0bb24a
giacomop1196 <giacomo.pillitteri65@gmail.com>
2025-11-03 14:14:09 +0100

    commit

--- mini-tracker/src/main/java/mini_tracker/mini_tracker/Exceptions/ValidationsException.java
+++ mini-tracker/src/main/java/mini_tracker/mini_tracker/Exceptions/ValidationsException.java
@@ -1,9 +1,23 @@
 package mini_tracker.mini_tracker.Exceptions;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.util.List;
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+@Getter
 public class ValidationsException extends RuntimeException {
-    public ValidationsException(List<String> message) {
-        super((Throwable) message);
+
+
+    private final List<String> errorsList;
+
+    public ValidationsException(List<String> errorsList) {
+
+        super("Errore di validazione nel payload");
+
+
+        this.errorsList = errorsList;
     }
 }
--- mini-tracker/src/main/java/mini_tracker/mini_tracker/Security/SecurityConfig.java
+++ mini-tracker/src/main/java/mini_tracker/mini_tracker/Security/SecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -24,7 +29,7 @@ public class SecurityConfig {
         httpSecurity.csrf(csrf -> csrf.disable());
         httpSecurity.sessionManagement(sessions ->
                 sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
+        httpSecurity.cors(cors -> cors.configurationSource(corsConfigurationSource()));
         httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         httpSecurity.authorizeHttpRequests(req -> req
@@ -33,5 +38,22 @@ public class SecurityConfig {
         );
 
         return  httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
