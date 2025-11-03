291ea46e25bfe8d49b85d37ed707f93c2f22cbd2
giacomop1196 <giacomo.pillitteri65@gmail.com>
2025-11-03 09:46:22 +0100

    first commit

---
+++ mini-tracker/src/main/java/mini_tracker/mini_tracker/MiniTrackerApplication.java
@@ -0,0 +1,13 @@
+package mini_tracker.mini_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTrackerApplication.class, args);
+	}
+
+}
---
+++ mini-tracker/src/test/java/mini_tracker/mini_tracker/MiniTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package mini_tracker.mini_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
